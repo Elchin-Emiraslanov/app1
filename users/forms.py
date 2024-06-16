502dff1ab3ee9b432b26ee34e299925f9e0f3cff
Elchin <emiraslanove575@gmail.com>
2024-06-16 17:24:35 +0400

    profile from controller & notofacitons are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.template.defaultfilters import first
 
 from users.models import User
 
@@ -87,3 +88,54 @@ class UserRegistrartion(UserCreationForm):
     #         }
     #     )
     # )
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+        image = forms.ImageField(required=False)
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.CharField()
+
+    # image = forms.ImageField(
+    #     widget=forms.FileInput(attrs={"class": "form-control mt-3 "}), required=False
+    # )
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs ={
+    #             "class": "form-control",
+    #             "place_holder": "Ведите ваше имя",
+    #         }
+    #     )
+    # )
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs ={
+    #             "class": "form-control",
+    #             "place_holder": "Ведите вашу фамилию",
+    #         }
+    #     )
+    # )
+    # username = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs ={
+    #             "class": "form-control",
+    #             "place_holder": "Ведите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # email = forms.CharField(
+    #     widget=forms.EmailInput(
+    #         attrs ={
+    #             "class": "form-control",
+    #             "place_holder": "Ведите ваше email *youremail@example.com",})
+    #             )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from users.forms import UserLoginForm, UserCreationForm, UserRegistrartion
+from traitlets import Instance
+from users.forms import ProfileForm, UserLoginForm, UserCreationForm, UserRegistrartion
 
 
 
@@ -18,6 +20,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Siz hesaba daxil olduz")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -36,6 +39,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Siz uğurla qeydiyyatdan keçdiz və daxil olduz")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrartion()
@@ -47,12 +51,25 @@ def registration(request):
     }
     return render( request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
+
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Səhifə uğurla yeniləndi!")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title' : 'Home - Cabinet'
+        'title' : 'Home - Cabinet',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
-
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Səhifədən çıxış edildi.")
     auth.logout(request)
     return redirect(reverse('main:index'))
