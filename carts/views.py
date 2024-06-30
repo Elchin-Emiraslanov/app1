9d5437ad29a9c74735ec234ec512555f3fbdde5e
Elchin <emiraslanove575@gmail.com>
2024-06-30 19:21:37 +0400

    Change cart ajax and json respomse, all ajax done

--- carts/views.py
+++ carts/views.py
@@ -37,7 +37,33 @@ def cart_add(request):
     return JsonResponse(response_data)
 
 def cart_change(request):
-    ...
+    if request.method == "POST":
+        cart_id = request.POST.get("cart_id")
+        quantity = request.POST.get("quantity")
+
+        print(f"Received cart_id: {cart_id}, quantity: {quantity}")
+
+        cart = Cart.objects.get(id=cart_id)
+        cart.quantity = quantity
+        cart.save()
+        updated_quantity = cart.quantity
+
+        user_cart = get_user_carts(request)
+        cart_items_html = render_to_string(
+            "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+        )
+
+        response_data = {
+            "message": "Quantity changed",
+            "cart_items_html": cart_items_html,
+            "quantity": updated_quantity
+        }
+
+        print("Response data:", response_data)
+
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({"error": "Invalid request method"}, status=400)
 
 
 
